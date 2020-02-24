b3a7cfb288caa41d480e3d03f4707bf0037d46ae
Caroline Pandolfe <carolinepandolfe@yahoo.com.br>
2020-02-24 10:33:30 -0300

    Config express

---
+++ pandoces-backend/app.js
@@ -0,0 +1,7 @@
+const customExpress = require('./config/customExpress')
+const app = customExpress()
+const port = '3000';
+
+app.listen(port, () =>{
+    console.log(`Rodando na porta ${port} `)
+})
