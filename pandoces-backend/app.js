6ac30e03f5aa875b2a100215e862b99497a365fb
Caroline Pandolfe <carolinepandolfe@yahoo.com.br>
2020-02-26 18:37:03 -0300

    Delete all

--- pandoces-backend/app.js
+++
@@ -1,7 +0,0 @@
-const customExpress = require('./config/customExpress')
-const app = customExpress()
-const port = '3000';
-
-app.listen(port, () =>{
-    console.log(`Rodando na porta ${port} `)
-})
