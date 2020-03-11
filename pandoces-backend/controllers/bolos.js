48c3996cb4493e83f6181c35743354610d639abd
Caroline Pandolfe <carolinepandolfe@yahoo.com.br>
2020-03-11 16:44:36 -0300

    Criar metodo post para rota bolos

--- pandoces-backend/controllers/bolos.js
+++ pandoces-backend/controllers/bolos.js
@@ -2,4 +2,9 @@ module.exports = app => {
     app.get("/bolos",(req, res)=> {
         res.json('Get dos bolos');
     })
+
+    app.post("/bolos", (req,res) => {
+        console.log(req.body);
+        res.json('Post dos bolos');
+    })
 }
