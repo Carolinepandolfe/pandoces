b7dc1f4a316aa5693d41feba54eddc6664d09739
Caroline Pandolfe <carolinepandolfe@yahoo.com.br>
2020-03-20 21:02:45 -0300

    Fix configuracao do firebase

--- pandoces-backend/controllers/bolos.js
+++ pandoces-backend/controllers/bolos.js
@@ -1,40 +1,12 @@
-let storageCloud = require('@google-cloud/storage')
-
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-let firebase = require("firebase/app");
-
-// Add the Firebase products that you want to use
-require("firebase/auth");
-require("firebase/firestore");
-let admin = require("firebase-admin");
-let serviceAccount = require("../service-account-file.json");
-
-// TODO: Replace the following with your app's Firebase project configuration
-let firebaseConfig = {
-    apiKey: "AIzaSyD1_qM6HcCnG_Bjqq672JM0FpvGtDmnxvc",
-    authDomain: "pandoces-cp.firebaseapp.com",
-    databaseURL: "https://pandoces-cp.firebaseio.com",
-    projectId: "pandoces-cp",
-    storageBucket: "pandoces-cp.appspot.com",
-    messagingSenderId: "859331037480",
-    appId: "1:859331037480:web:6241c1ea9951751042681d",
-    measurementId: "G-HD55D1749M"
-  };
-    admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://pandoces-cp.firebaseio.com"
-    });
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  // Get a reference to the storage service, which is used to create references in your storage bucket
-  let storage = admin.storage();
-
 module.exports = app => {
+
+      app.on('error', (error) =>{
+        console.log(error)
+    })    
+
     app.get("/bolos",(req, res)=> {
         res.json('Get dos bolos');
+        console.log('oi');
     })
 
     app.post("/bolos", (req,res) => {
--- pandoces-backend/server.js
+++ pandoces-backend/server.js
@@ -1,8 +1,25 @@
 const customExpress = require('./config/customExpress')
 const app = customExpress();
 const port = 3000;
+const admin = require('firebase-admin');
+const serviceAccount = require("./service-account-file.json");
 
-app.on(port, () =>{
+let firebaseConfig = {
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://pandoces-cp.firebaseio.com",
+    apiKey: "AIzaSyD1_qM6HcCnG_Bjqq672JM0FpvGtDmnxvc",
+    authDomain: "pandoces-cp.firebaseapp.com",
+    databaseURL: "https://pandoces-cp.firebaseio.com",
+    projectId: "pandoces-cp",
+    storageBucket: "pandoces-cp.appspot.com",
+    messagingSenderId: "859331037480",
+    appId: "1:859331037480:web:6241c1ea9951751042681d",
+    measurementId: "G-HD55D1749M"
+  };
+
+admin.initializeApp({firebaseConfig});
+
+app.listen(port, () =>{
     console.log(`Rodando na Porta ${port}`)
 })
 
